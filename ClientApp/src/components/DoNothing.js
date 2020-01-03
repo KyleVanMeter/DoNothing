7cd3389e875588815d9ae80d5f264c53dafd2539
Kyle Van Meter <kvanmete76751@gmail.com>
2020-01-03 12:22:06 -0700

    An ever so slightly changed version of the ASP.NET + React template from VS2019

---
+++ ClientApp/src/components/DoNothing.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class DoNothing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi!</h1>
+                <p>This is HTML</p>
+            </div>
+        );
+    }
+}
