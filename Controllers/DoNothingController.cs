43f318d9e5809b7a80fdb91ec42105786c1e2135
Kyle Van Meter <kvanmete76751@gmail.com>
2020-01-10 17:49:28 -0700

    Added Tracks/Album interface in DataAccessLayer

--- Controllers/DoNothingController.cs
+++ Controllers/DoNothingController.cs
@@ -3,15 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using test02.Models;
 
 using TagLib;
 
 namespace test02.Controllers
 {
+    interface IAlbum
+    {
+        IEnumerable<Album> Index();
+        Album Get(int id);
+        List<Tracks> GetTracks(int id);
+        void Add(Album album);
+        void Update(Album album);
+    }
+    interface ITracks
+    {
+        IEnumerable<Tracks> Index();
+        Tracks Get(int id);
+        void Add(Tracks track);
+        void Update(Tracks track);
+        
+    }
+
     [ApiController]
     [Route("[controller]")]
-    public class DoNothingController : ControllerBase
+    public class DoNothingController : ControllerBase, IAlbum, ITracks
     {
+        DataAccessLayer data = new DataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Album/Index")]
+        IEnumerable<Album> IAlbum.Index()
+        {
+            return data.GetAllAlbums();
+        }
+
+        [HttpGet]
+        [Route("api/Album/Get/{id}")]
+        Album IAlbum.Get(int id)
+        {
+            return data.GetAlbumById(id);
+        }
+
+        [HttpGet]
+        [Route("api/Album/Tracks/{id}")]
+        List<Tracks> IAlbum.GetTracks(int id)
+        {
+            return data.GetTracksFromAlbum(id);
+        }
+
+        [HttpPost]
+        [Route("api/Album/Add")]
+        void IAlbum.Add(Album album)
+        {
+            data.AddAlbum(album);
+        }
+
+        [HttpPut]
+        [Route("api/Album/Update")]
+        void IAlbum.Update(Album album)
+        {
+            data.UpdateAlbum(album);
+        }
+
+        [HttpGet]
+        [Route("api/Tracks/Index")]
+        IEnumerable<Tracks> ITracks.Index()
+        {
+            return data.GetAllTracks();
+        }
+
+        [HttpGet]
+        [Route("api/Tracks/Get/{id}")]
+        Tracks ITracks.Get(int id)
+        {
+            return data.GetTrackById(id);
+        }
+
+        [HttpPost]
+        [Route("api/Tracks/Add")]
+        void ITracks.Add(Tracks track)
+        {
+            data.AddTrack(track);
+        }
+
+        [HttpPut]
+        [Route("api/Tracks/Update")]
+        void ITracks.Update(Tracks track)
+        {
+            data.UpdateTrack(track);
+        }
+
         [HttpGet]
         public IEnumerable<DoNothing> Get()
         {
