dfba791a5eb659bd34522c7220465628514c2866
Kyle Van Meter <kvanmete76751@gmail.com>
2020-02-04 18:11:59 -0700

    Track/Album response conversion

--- DoNothing.cs
+++ DoNothing.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using TagLib;
 using System.Collections.Generic;
+using System.Linq;
+
+using test02.Models;
+
 
 namespace test02
 {
     public class TrackResponse
     {
+        public TrackResponse() { }
+        public TrackResponse(Tracks tracks)
+        {
+            Disk = tracks.Disk;
+            TrackNumber = tracks.TrackNumber;
+            TrackTitle = tracks.TrackTitle;
+            TrackArtist = tracks.TrackArtist;
+            Duration = tracks.Duration;
+            Path = tracks.Path;
+        }
         public int? Disk { get; set; }
         public int TrackNumber { get; set; }
         public string TrackTitle { get; set; }
@@ -15,6 +29,14 @@ namespace test02
     }
     public class AlbumResponse
     {
+        public AlbumResponse() { }
+        public AlbumResponse(Album album)
+        {
+            AlbumName = album.AlbumTitle;
+            Artists = album.AlbumArtist;
+            Year = album.Year;
+            Tracks = album.Tracks.Select(x => new TrackResponse(x)).ToList();
+        }
         public string AlbumName { get; set; }
         public string Artists { get; set; }
         public int Year { get; set; }
